f478a51ab1a68eb074bcba25ff658e25124b5bff
Wisdom <mawuliadzokatse@gmail.com>
2023-02-21 19:37:26 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "alx africa.h"
+
+/**
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
