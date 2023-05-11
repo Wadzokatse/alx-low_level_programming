5d57051246cf1517a9b67635e4ce051cd4114b48
Wisdom <mawuliadzokatse@gmail.com>
2023-05-10 23:58:33 -0700

    correction of task one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,32 @@
  *
  * Return: 1 if it success. -1 if it fails.
  */
+int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int area = 0, base = 0;
-	char *ptr;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (base = 0, ptr = text_content; *ptr; ptr++)
-			base++;
-	area = write(file, text_content, inlen);
-	}
+	if (!text_content)
+		text_content = "";
 
-	if (close(file) == -1 || base != area)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
+
+	close(fd);
+
 	return (1);
 }
