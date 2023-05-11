80a1d924809b4b55b409744dea6131f850645181
Wisdom <mawuliadzokatse@gmail.com>
2023-05-10 23:51:18 -0700

    correction of task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,35 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * create_file - creates a file
  *
  * Return: 1 if it success. -1 if it fails.
  */
-int create_file(const char *filename, char *text_content)
 {
-	int al;
-	int nletters;
-	int rwr;
+	int file;
+	int area = 0, base = 0;
+	char *ptr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	al = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (al == -1)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content != NULL)
+	{
+		for (base = 0, ptr = text_content; *ptr; ptr++)
+			base++;
+	area = write(file, text_content, inlen);
+	}
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	if (close(file) == -1 || base != area)
 		return (-1);
-
-	close(al);
-
 	return (1);
 }
